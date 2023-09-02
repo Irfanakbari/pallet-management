9764b7aa2743eb11a9eed4a3cb4acb7e90a78877
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-09-02 12:33:21 +0700

    ~~

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,19 +1,18 @@
-import { useStoreTab } from "@/context/states";
-
 import {Dropdown} from "antd";
+import {useStoreTab} from "@/context/states";
 
 // Komponen MainMenu menerima prop title dan data
-export default function MainMenu({ title, data }) {
-    const { setNewTab } = useStoreTab();
-    return (
-        <Dropdown
-            menu={{
-                items:data,
-                onClick: (d)=>setNewTab(d.key)
-            }}
-            placement="bottom"
-        >
-            <span className={`border-gray-500 border-r hover:bg-[#85d3ff] hover:cursor-pointer px-4`}>{title}</span>
-        </Dropdown>
-    );
+export default function MainMenu({title, data}) {
+	const {setNewTab} = useStoreTab();
+	return (
+		<Dropdown
+			menu={{
+				items: data,
+				onClick: (d) => setNewTab(d.key)
+			}}
+			placement="bottom"
+		>
+			<span className={`border-gray-500 border-r hover:bg-[#85d3ff] hover:cursor-pointer px-4`}>{title}</span>
+		</Dropdown>
+	);
 }
